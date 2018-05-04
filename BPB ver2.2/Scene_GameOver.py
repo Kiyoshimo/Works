79853ec63cc669a6336952df0510b5816442bc41
三之轮银 <jason.asuka@gmail.com>
2018-05-04 23:54:55 +0800

    游戲設計

--- BPB ver2.2/Scene_GameOver.py
+++
@@ -1,8 +0,0 @@
-import pygame
-import sqlite3
-
-class Scene_GameOver:
-    def __init__(self, gamemode = 1, playerdata = (0,0), extradata = []):
-        self.gamemode = gamemode
-        maxcombo, score = playerdata        
-        
